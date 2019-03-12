6b2b70cc490df060b98655ff2a0395eb731aec9c
klx3300 <inc19991@gmail.com>
2019-03-12 11:18:55 +0800

    Initial commit, should partially work now..

---
+++ config.h
@@ -0,0 +1,22 @@
+/*
+ *  THIS FILE IS AUTOMATICALLY CREATED BY configure!
+ *  DON'T EDIT THIS FILE MANUALLY, IT WILL BE OVERWRITTEN.
+ */
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define VERSION "0.6.1"
+#define WITHUNITTESTS
+#define COMPILE_DATE "compiled on Linux/x86_64, Tue Mar 12 10:57:08 CST 2019"
+#define ADD_ALL_CPU_FAMILIES     add_cpu_family(alpha_cpu_family_init, ARCH_ALPHA); add_cpu_family(arm_cpu_family_init, ARCH_ARM); add_cpu_family(m88k_cpu_family_init, ARCH_M88K); add_cpu_family(mips_cpu_family_init, ARCH_MIPS); add_cpu_family(ppc_cpu_family_init, ARCH_PPC); add_cpu_family(sh_cpu_family_init, ARCH_SH);
+#define WITH_X11
+#define strlcpy mystrlcpy
+#define strlcat mystrlcat
+#define USE_STRLCPY_REPLACEMENTS
+#define HAVE___FUNCTION__
+#define HOST_LITTLE_ENDIAN
+
+#undef mips
+
+#endif  /*  CONFIG_H  */
