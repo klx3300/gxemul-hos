000fbb834dc94b6373f74e63bfbee0e8d134548d
klx3300 <inc19991@gmail.com>
2019-03-15 01:33:39 +0800

    fixed the TLB problem

--- config.h
+++ config.h
@@ -9,7 +9,7 @@
 #define VERSION "hos"
 #define UNSTABLE_DEVEL
 #define WITHUNITTESTS
-#define COMPILE_DATE "compiled on Linux/x86_64, Thu Mar 14 14:17:12 CST 2019"
+#define COMPILE_DATE "compiled on Linux/x86_64, Fri Mar 15 01:14:48 CST 2019"
 #define ADD_ALL_CPU_FAMILIES     add_cpu_family(alpha_cpu_family_init, ARCH_ALPHA); add_cpu_family(arm_cpu_family_init, ARCH_ARM); add_cpu_family(m88k_cpu_family_init, ARCH_M88K); add_cpu_family(mips_cpu_family_init, ARCH_MIPS); add_cpu_family(ppc_cpu_family_init, ARCH_PPC); add_cpu_family(sh_cpu_family_init, ARCH_SH);
 #define WITH_X11
 #define strlcpy mystrlcpy
